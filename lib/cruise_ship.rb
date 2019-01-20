b6d25d75933cfc3e85ecfe041021b2f61da877fb
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-20 02:35:17 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,5 +9,9 @@
 def select_winner(passengers)
   # add the code snippet here!
   winner = ""
-  
+  passengers.each do |key, value|
+  if key == :suite_a && value[0]=="A"
+    winner = value
+  end
+end
 end
